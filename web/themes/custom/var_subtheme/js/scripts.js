4b896a83c40e3f8ae0ddf2b9d8654289964b5ae0
Sakshi Sharma <sakshi@deosi.com>
2021-12-30 16:42:28 +0530

    Initial site setup

---
+++ web/themes/custom/var_subtheme/js/scripts.js
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * Behaviors for the var_subtheme theme.
+ */
+
+(function ($, _, Drupal) {
+  Drupal.behaviors.var_subtheme = {
+    attach: function () {
+      // var_subtheme JavaScript behaviors goes here.
+    }
+  };
+})(window.jQuery, window._, window.Drupal);
